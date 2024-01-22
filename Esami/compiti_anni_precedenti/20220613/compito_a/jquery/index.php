e2934a1103beabeb9bac6e9bd0450b00fb9f0aab
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2024-01-22 18:11:52 +0100

    Finisched CSS Exercise and learned how to request a file .php rom JS

---
+++ Esami/compiti_anni_precedenti/20220613/compito_a/jquery/index.php
@@ -0,0 +1,12 @@
+<?php
+    //echo "Michelis";
+    $array = array("prova", "prova2");
+    // json encode serve per creare un JSON partendo da un'array
+    //json_encode($array);
+    //$_REQUEST['q'] = $array;
+
+    // get prende i valori passati dal JS passato nella variabile q
+    var_dump($_GET['q']);
+
+    // per passare la risposta da JS a PHP scrivi echo "valore che vuoi passare"
+?>
--- Esami/compiti_anni_precedenti/20220613/compito_a/jquery/soluzione.js
+++ Esami/compiti_anni_precedenti/20220613/compito_a/jquery/soluzione.js
@@ -13,11 +13,26 @@ document.addEventListener("DOMContentLoaded", function() {
     let b_new = document.getElementsByTagName("button")[0];
     
     b_new.addEventListener("click", function() {
+        /*
         $.ajax({
             url: "index.php",
+            data: {
+                type: "new_game"
+            },
             success: function (data) {
                 console.log(data);
             }
         });
+        */
+       let str = "new_game";
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            //document.getElementById("txtHint").innerHTML = this.responseText;
+            console.log(this.responseText);
+          }
+        };
+        xmlhttp.open("GET", "index.php?q=" + str, true);
+        xmlhttp.send();
     });
 });
