70ca64eaeae807cbbea7cb2659afd7d09f0f2854
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2024-01-29 19:03:46 +0100

    Updated JS and PHP Exercise of 2022-06-13

--- Esami/compiti_anni_precedenti/20220613/compito_a/jquery/soluzione.js
+++ Esami/compiti_anni_precedenti/20220613/compito_a/jquery/soluzione.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let form = document.getElementsByTagName("form")[0];
     let array_span = document.getElementsByTagName("span");
     let button = document.getElementsByTagName("button")[1];
+    let add = document.getElementsByName("submit")[0];
 
     form.setAttribute("hidden", "hidden");
     button.setAttribute("hidden", "hidden");
@@ -13,6 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     let b_new = document.getElementsByTagName("button")[0];
+    const rows = 9;
+    const columns = 9;
+    let my_table = new Map();
     
     b_new.addEventListener("click", function() {
         /*
@@ -30,11 +34,71 @@ document.addEventListener("DOMContentLoaded", function() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            //document.getElementById("txtHint").innerHTML = this.responseText;
-            console.log(this.responseText);
+            //console.log(this.responseText);
+            let json_response = JSON.parse(this.responseText);
+            
+            let status = json_response.statoiniziale;
+
+            //console.log(status[5]);
+            let counter = 0;
+
+
+            for (let i = 0; i < rows; i++) {
+                for (let j = 0; j < columns; j++) {
+                    my_table.set(`${i + 1} ${j + 1}`, `${status[counter]}`);
+                    counter++;
+                }
+            }
+
+            generate_table();
+            console.log(my_table);
+
+            form.removeAttribute("hidden");
+            form.setAttribute("name","myForm");
+            function handleForm(event) { event.preventDefault(); } 
+            form.addEventListener('submit', handleForm);
+            add.addEventListener("click", function () {
+                //console.log(document.forms["myForm"]["riga"].value);
+                let row = Number(document.forms["myForm"]["riga"].value);
+                let column = Number(document.forms["myForm"]["colonna"].value);
+                let my_value = Number(document.forms["myForm"]["valore"].value);
+                if ((row >= 1 && row <= rows) && (column >= 1 && column <= columns)) {
+                    if ( my_value >= 0 && my_value <= 9) {
+                        console.log(typeof my_value);
+                        my_table.set(`${row} ${column}`, my_value);
+                        //console.log(my_table);
+                        generate_table();
+                    }
+                }
+            });
+
+            //form.removeAttribute("hidden");
+            //form.setAttribute("name","myForm");
+            //add.addEventListener("click", function () {
+            //    //console.log(document.forms["myForm"]["riga"].value);
+            //});
+
           }
         };
-        xmlhttp.open("GET", "../php/index.php?q=" + str, true);
+        xmlhttp.open("GET", "../php/index.php?message=" + str, true);
         xmlhttp.send();
+
+        console.log("fine");
+
     });
+
+    function generate_table() {
+        let table = document.getElementsByTagName("table")[0];
+        let content_table = ``;
+
+        for (let i = 1; i < rows + 1; i++) {
+            content_table += `<tr>`;
+            for (let j = 1; j < columns + 1; j++) {
+                content_table += `<td data="${i} ${j}"> ${my_table.get(`${i} ${j}`)} </td>`;
+            }
+            content_table += `</tr>`;
+        }
+
+        table.innerHTML = content_table;
+    }
 });
--- Esami/compiti_anni_precedenti/20220613/compito_a/php/index.php
+++ Esami/compiti_anni_precedenti/20220613/compito_a/php/index.php
@@ -1,14 +1,36 @@
 <?php
 
-    //echo "Michelis";
-    $array = array("prova", "prova2");
+    //$array = array("prova", "prova2");
     // json encode serve per creare un JSON partendo da un'array
     //json_encode($array);
-    //$_REQUEST['q'] = $array;
 
-    // get prende i valori passati dal JS passato nella variabile q
-    var_dump($_GET['q']);
+    // get prende i valori passati dal JS passato nella variabile 'message'
+    //var_dump($_GET['message']);
+
+    $dbname = "esami";
+    // Create connection
+    $conn = new mysqli("localhost", "root", "", $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    } else {
+        //echo "Connection Success";
+        if ($_GET['message'] == "new_game") {
+            //echo "New game starto";
+            $query = "SELECT * FROM sudoku";
+            // prepare and bind
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $status = $result->fetch_all(MYSQLI_ASSOC)[0];
+
+            // json encode serve per creare un JSON partendo da un'array
+            $json_status = json_encode($status);
+            // qui mando al JS il JSON creato
+            echo "".$json_status;
+        }
+    }
 
     // per passare la risposta da PHP a JS scrivi echo "valore che vuoi passare"
-    echo "mando roba";
+    //echo "mando roba";
 ?>
